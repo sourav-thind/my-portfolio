e2f3a107516f68a7d94a531717641de407baa00d
Sourav-thind <souravthind07it@gmail.com>
2025-02-25 23:45:19 -0400

    Html Landing reimposed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 
 import './App.css';
+import HtmlMain from './Components/Html Site/HtmlMain';
 import MainCanvas from './Components/Maincanvas/MainCanvas';
 import StatsComponent from './Components/Stats';
 
 function App() {
   return (
     <div className="App">
-     <MainCanvas/>
+      <HtmlMain/>
+     {/* <MainCanvas/>
      <div className='top-0'>
 
      <StatsComponent/>
-     </div>
+     </div> */} 
     </div>
   );
 }
